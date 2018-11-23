f6b96b686a1c2b80b439a8d9325c8550f92f62c3
ShivaniKomandury <44065594+ShivaniKomandury@users.noreply.github.com>
2018-11-23 10:08:28 +0530

    Exp-1 Backup No-eval New

--- src/lab/exp1/quizconfig.js
+++ src/lab/exp1/quizconfig.js
@@ -61,10 +61,10 @@ function showsolution(){
 var win2=window.open("","win2","width=200,height=350, scrollbars")
 win2.focus()
 win2.document.open()
-win2.document.write('<title>Solution</title>');
-win2.document.write('<body bgcolor="#FFFFFF">');
-win2.document.write('<center><h3>Solution to Quiz</h3></center>');
-win2.document.write('<center><font face="Arial">');
+win2.document.write("<title>Solution</title>");
+win2.document.write("<body bgcolor="#FFFFFF">");
+win2.document.write("<center><h3>Solution to Quiz</h3></center>");
+win2.document.write("<center><font face="Arial">");
 for (i=1;i<=totalquestions;i++){
 for (temp=0;temp<incorrect.length;temp++){
 if (i===incorrect[temp]){
@@ -76,10 +76,10 @@ win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
 wrong=0;
 }
 else{
-win2.document.write("Question "+i+"="+correctchoices[i]+"<br>")
+win2.document.write("Question "+i+"="+correctchoices[i]+"<br>");
 }
 }
-win2.document.write('</center></font>')
-win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5>")
-win2.document.close()
+win2.document.write("</center></font>");
+win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5>");
+win2.document.close();
 }
